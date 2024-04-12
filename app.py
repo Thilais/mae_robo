bffc01e003c16feb9799d1e10be090952ca0bb0f
Thilais <163068904+Thilais@users.noreply.github.com>
2024-04-11 21:59:21 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from flask import Flask, request
+import openai
+from youtube_transcript_api import YouTubeTranscriptApi
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+import time
+import re
+import requests
+from datetime import datetime
+import time
+
+app = Flask(__name__)
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+
+@app.route("/")
+def index():
+    return "Oi Thi<3"
+
+
+@app.route("/telegram", methods=["POST"])
+def telegram_update():
+    update = request.json
+    url_envio_mensagem = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    chat_id = update["message"]["chat"]["id"]
+    mensagem = {"chat_id": chat_id, "text": "mensagem <b>recebida</b>!", "parse_mode": "HTML"}
+    requests.post(url_envio_mensagem, data=mensagem)
+    return "ok"
